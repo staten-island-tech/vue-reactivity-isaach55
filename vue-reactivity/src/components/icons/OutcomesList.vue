e42e46bdf38c2709eb4561df6a5b2a8fef110b2a
isaach55 <isaach55@nycstudents.net>
2025-02-05 12:30:13 -0500

    feedback ideas

---
+++ vue-reactivity/src/components/icons/OutcomesList.vue
@@ -0,0 +1,27 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+/*banana
+all p flour, bread flour? -> this is so dry are you making banana bread or mixing concrete
+egg -> that much egg? in this economy?
+vanilla -> thats a lot of vanilla relax
+sugar -> 
+honey ->      }}} if sweetness > something -> erm thats kind of a lot of sugar ERM thats kind of a LOT of sugar ERM THATS A LOT OF SuGAR
+maple syrup ->
+butter / oil -> your banana bread is so oily the united states military would drill in it
+chocolate chips -> uhhh you got some banana in your chocolate bread thats more chocolate chips than samantha has hairs on her head
+cinnamon -> 
+milk -> 
+salt -> this is almost as salty as you would be if you had Samantha as a teammate
+
+winnie at your doorstep name 5 things that you could have done better just hand over the banana bread already
+the only person that could make banana bread worse than this is samantha
+
+IDEAS:
+sliders, interactive feedback?
+*/
+</script>
+
+<style lang="scss" scoped></style>
